c27bed4b5d34c401c15486fea35f6d9ff1c7e33f
Sam LEE <63564804+MacTechIN@users.noreply.github.com>
2023-10-04 17:38:30 +0900

    Initial Commit

---
+++ Apple-Frameworks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple-Frameworks
+//
+//  Created by SamLEE on 2023/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
